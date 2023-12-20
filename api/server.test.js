1fc9acea4df5874ef09f6583aa35aa07bcdef7e9
Justin Byrd <justinbyrd7@gmail.com>
2023-12-20 16:04:33 -0600

    Update test scripts and add API endpoint tests

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,25 @@
+const request = require("supertest");
+const server = require("./server.js");
+const db = require("../data/dbConfig.js");
+
 // Write your tests here
 test("sanity", () => {
-  expect(true).toBe(false);
+  expect(true).toBe(true);
+});
+
+//// A minimum of 2 tests per API endpoint ////
+
+describe("[GET] /api/jokes", () => {
+  test.todo("returns a 200 OK status code");
+  test.todo("returns a JSON object");
+});
+
+describe("[POST] /api/auth/register", () => {
+  test.todo("returns a 201 OK status code");
+  test.todo("returns a JSON object");
+});
+
+describe("[POST] /api/auth/login", () => {
+  test.todo("returns a 200 OK status code");
+  test.todo("Welcome, ${username}");
 });
