29c458d563c79d8543557247a485f64e831d1ec1
Justin Byrd <justinbyrd7@gmail.com>
2023-12-20 09:45:41 -0600

    Update auth-router.js with registration and login functionality

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -11,13 +11,13 @@ router.post("/register", (req, res) => {
     You are welcome to build additional middlewares to help with the endpoint's functionality.
     DO NOT EXCEED 2^8 ROUNDS OF HASHING!
 
-    1- In order to register a new account the client must provide `username` and `password`:
+    *1- In order to register a new account the client must provide `username` and `password`:
       {
         "username": "Captain Marvel", // must not exist already in the `users` table
         "password": "foobar"          // needs to be hashed before it's saved
       }
 
-    2- On SUCCESSFUL registration,
+    *2- On SUCCESSFUL registration,
       the response body should have `id`, `username` and `password`:
       {
         "id": 1,
@@ -25,10 +25,10 @@ router.post("/register", (req, res) => {
         "password": "2a$08$jG.wIGR2S4hxuyWNcBf9MuoC4y0dNy7qC/LbmtuFBSdIhWks2LhpG"
       }
 
-    3- On FAILED registration due to `username` or `password` missing from the request body,
+    *3- On FAILED registration due to `username` or `password` missing from the request body,
       the response body should include a string exactly as follows: "username and password required".
 
-    4- On FAILED registration due to the `username` being taken,
+    *4- On FAILED registration due to the `username` being taken,
       the response body should include a string exactly as follows: "username taken".
   */
 });
@@ -39,23 +39,23 @@ router.post("/login", (req, res) => {
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
 
-    1- In order to log into an existing account the client must provide `username` and `password`:
+    *1- In order to log into an existing account the client must provide `username` and `password`:
       {
         "username": "Captain Marvel",
         "password": "foobar"
       }
 
-    2- On SUCCESSFUL login,
+    *2- On SUCCESSFUL login,
       the response body should have `message` and `token`:
       {
         "message": "welcome, Captain Marvel",
         "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
       }
 
-    3- On FAILED login due to `username` or `password` missing from the request body,
+    *3- On FAILED login due to `username` or `password` missing from the request body,
       the response body should include a string exactly as follows: "username and password required".
 
-    4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
+    *4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
       the response body should include a string exactly as follows: "invalid credentials".
   */
 });
